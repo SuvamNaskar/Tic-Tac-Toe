f02124614e64e48a562a3de6881877d78456a395
Suvam Naskar <132764522+SuvamNaskar@users.noreply.github.com>
2023-08-17 06:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,35 @@ let flag = 0;
 let p;
 let xo = null;
 
-
-let trackerX = [];
-let trackerO = [];
+let c1 ,c2, c3, c4, c5, c6, c7, c8, c9;
 
 
 function checkWin()
 {
-    
-
-    trackerO.sort(function(a, b){return a-b});
-    trackerX.sort(function(a, b){return a-b});
-
+    if(c1 == "X" && c2 == "X" && c3 == "X"
+        || c4 == "X" && c5 == "X" && c6 == "X"
+        || c7 == "X" && c8 == "X" && c9 == "X"
+        || c1 == "X" && c4 == "X" && c7 == "X"
+        || c2 == "X" && c5 == "X" && c8 == "X"
+        || c3 == "X" && c6 == "X" && c9 == "X"
+        || c1 == "X" && c5 == "X" && c9 == "X"
+        || c3 == "X" && c5 == "X" && c7 == "X")
+    {
+        //player 1 wins
+        document.getElementById("win").innerHTML = "Player 1 Won";
+    }
+    elseif(c1 == "O" && c2 == "O" && c3 == "O"
+        || c4 == "O" && c5 == "O" && c6 == "O"
+        || c7 == "O" && c8 == "O" && c9 == "O"
+        || c1 == "O" && c4 == "O" && c7 == "O"
+        || c2 == "O" && c5 == "O" && c8 == "O"
+        || c3 == "O" && c6 == "O" && c9 == "O"
+        || c1 == "O" && c5 == "O" && c9 == "O"
+        || c3 == "O" && c5 == "O" && c7 == "O")
+    {
+        //player 2 wins
+        document.getElementById("win").innerHTML = "Player 2 Won";
+    }
 }
 
 function checkFlag()
@@ -39,6 +56,7 @@ function mark1()
     xo = getSymbol();
     document.getElementById("1").innerHTML = xo;
     document.getElementById("1").disabled = true;
+    c1 = xo;
     changeFlag();
     checkWin();
 }
@@ -48,6 +66,7 @@ function mark2()
     xo = getSymbol();
     document.getElementById("2").innerHTML = xo;
     document.getElementById("2").disabled = true;
+    c2 = xo;
     changeFlag();
     checkWin();
 }
@@ -57,6 +76,7 @@ function mark3()
     xo = getSymbol();
     document.getElementById("3").innerHTML = xo;
     document.getElementById("3").disabled = true;
+    c3 = xo;
     changeFlag();
     checkWin();
 }
@@ -66,6 +86,7 @@ function mark4()
     xo = getSymbol();
     document.getElementById("4").innerHTML = xo;
     document.getElementById("4").disabled = true;
+    c4 = xo;
     changeFlag();
     checkWin();
 }
@@ -75,6 +96,7 @@ function mark5()
     xo = getSymbol();
     document.getElementById("5").innerHTML = xo;
     document.getElementById("5").disabled = true;
+    c5 = xo;
     changeFlag();
     checkWin();
 }
@@ -84,6 +106,7 @@ function mark6()
     xo = getSymbol();
     document.getElementById("6").innerHTML = xo;
     document.getElementById("6").disabled = true;
+    c6 = xo;
     changeFlag();
     checkWin();
 }
@@ -93,6 +116,7 @@ function mark7()
     xo = getSymbol();
     document.getElementById("7").innerHTML = xo;
     document.getElementById("7").disabled = true;
+    c7 = xo;
     changeFlag();
     checkWin();
 }
@@ -102,6 +126,7 @@ function mark8()
     xo = getSymbol();
     document.getElementById("8").innerHTML = xo;
     document.getElementById("8").disabled = true;
+    c8 = xo;
     changeFlag();
     checkWin();
 }
@@ -111,6 +136,7 @@ function mark9()
     xo = getSymbol();
     document.getElementById("9").innerHTML = xo;
     document.getElementById("9").disabled = true;
+    c9 = xo;
     changeFlag();
     checkWin();
 }
